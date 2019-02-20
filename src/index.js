cb1102541b3d81d270ba8f869932d7270f20f0d8
Ali Lavska <46864916+alinonax@users.noreply.github.com>
2019-02-20 19:35:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let h = 50, q = 25, d = 10, n = 5, p = 1;
+    let result = {};
+
+    if (currency <= 0) {
+    	return result;
+    };
+
+    if (currency >= 10000) {
+    	return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    };
+
+    // h
+    let num_h_coins = Math.floor(currency / h);
+    if (num_h_coins > 0) {
+    	result['H'] = num_h_coins;
+    };
+
+    // q
+    let num_q_coins = Math.floor((currency - num_h_coins * h) / q);
+    if (num_q_coins > 0) {
+    	result['Q'] = num_q_coins;
+    };
+
+    // d
+    let num_d_coins = Math.floor((currency - num_h_coins * h - num_q_coins * q) / d);
+    if (num_d_coins > 0) {
+    	result['D'] = num_d_coins;
+    };
+
+    // n
+    let num_n_coins = Math.floor((currency - num_h_coins * h - num_q_coins * q - num_d_coins * d) / n);
+    if (num_n_coins > 0) {
+    	result['N'] = num_n_coins;
+    };
+    
+    // p
+    let num_p_coins = Math.floor(currency - num_h_coins * h - num_q_coins * q - num_d_coins * d - num_n_coins * n);
+    if (num_p_coins > 0) {
+    	result['P'] = num_p_coins;
+    };
+
+    return result;
 }
